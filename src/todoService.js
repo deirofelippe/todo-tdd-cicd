99e89b22091e8dbb1261711bad1f875577ae9a30
felippedesouza <sergiofelippe.deiro@gmail.com>
2021-12-13 19:10:55 -0300

    list do todoService

---
+++ src/todoService.js
@@ -0,0 +1,13 @@
+class TodoService {
+   constructor({ todoRepository }) {
+      this.todoRepository = todoRepository
+   }
+
+   create(todoItem) { }
+
+   list(query) {
+      return this.todoRepository.list().map(({ meta, $loki, ...result }) => result)
+   }
+}
+
+module.exports = TodoService
